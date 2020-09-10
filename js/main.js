964d59b06e5d7d216f1460634d0720da35778bd3
SM Boolean <samuelemadrigali1991@gmail.com>
2020-09-10 11:42:51 +0200

    Aggiunto Milestone 2

--- js/main.js
+++ js/main.js
@@ -14,6 +14,28 @@ $(document).ready(function() {
     }
   );
 
+  $("#search").keyup(
+    function() {
+
+      var searchInput = $(this).val();
+      searchInput = searchInput.toLowerCase();
+
+      var contactsName = $(".contact .contact-name");
+
+      contactsName.each(function() {
+        var name = $(this).text();
+        name = name.toLowerCase();
+
+        if(name.includes(searchInput) == true) {
+          $(this).parents(".contact").show();
+        } else {
+          $(this).parents(".contact").hide();
+        }
+      });
+
+    }
+  );
+
 });
 
 function sendMessage() {
@@ -22,16 +44,36 @@ function sendMessage() {
   if(inputText != "") {
     var templateMessage = $(".templates .message-row").clone();
 
-    var date = new Date();
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var time = hours + ":" + minutes;
+    var time = getTime();
 
     templateMessage.find(".message-text").text(inputText);
     templateMessage.find(".message-time").text(time);
     templateMessage.addClass("sent");
 
     $(".chat").append(templateMessage);
+    setTimeout(cpuMessage, 1000);
     $("#input-message").val("");
   }
+}
+
+function cpuMessage() {
+  var cpuMessage = $(".templates .message-row").clone();
+
+  cpuMessage.find(".message-text").text("Ok");
+  var time = getTime();
+  cpuMessage.find(".message-time").text(time);
+
+  $(".chat").append(cpuMessage);
+}
+
+function getTime() {
+  var date = new Date();
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+
+  if(minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  return hours + ":" + minutes;
 }
